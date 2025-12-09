b91116da6add02d5cf52445e0fc8eb31a49036b7
Fernando Nunes <fn1@stpauls.br>
2025-12-09 19:34:03 -0300

    added the main classes and set up one for testing

--- src/main/java/com/compsciia/compsciia/ConnectionFactory.java
+++ src/main/java/com/compsciia/compsciia/ConnectionFactory.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.compsciia.compsciia;
 
 /**
  *
  * @author fernandonunes
  */
-public class CompSciIA {
-
-    public static void main(String[] args) {
-        System.out.println("testing netbeans");
-    }
+public class ConnectionFactory {
+    
 }
