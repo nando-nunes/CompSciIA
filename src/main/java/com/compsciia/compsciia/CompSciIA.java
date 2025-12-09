326befc1088cbd9d34aaf8f151b0e97670e2e1af
Fernando Nunes <fn1@stpauls.br>
2025-12-09 19:25:13 -0300

    Initial commit

---
+++ src/main/java/com/compsciia/compsciia/CompSciIA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.compsciia.compsciia;
+
+/**
+ *
+ * @author fernandonunes
+ */
+public class CompSciIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
